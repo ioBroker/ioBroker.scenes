71ac8effa12ad6e3f1e30891cc4f41986bb71392
GermanBluefox <dogafox@gmail.com>
2020-06-14 19:53:21 +0200

    Fix connection

---
+++ admin/precache-manifest.d2bfb8575fdfbf0f316357a1f0509f65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c74166b3535650ba278e675fd13cfdc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "911ae862f51172da5183",
+    "url": "./static/css/2.9ba4c952.chunk.css"
+  },
+  {
+    "revision": "2ee3c9437901b9a4f03e",
+    "url": "./static/css/main.d4bed9c6.chunk.css"
+  },
+  {
+    "revision": "911ae862f51172da5183",
+    "url": "./static/js/2.cf262504.chunk.js"
+  },
+  {
+    "revision": "799f3d5c27694f0f3c92bbc8286bf099",
+    "url": "./static/js/2.cf262504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee3c9437901b9a4f03e",
+    "url": "./static/js/main.d53f9805.chunk.js"
+  },
+  {
+    "revision": "7efe789d81385b014df3",
+    "url": "./static/js/runtime-main.8cdd9b0f.js"
+  }
+]);
--- admin/service-worker.js
+++ admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.43b7b99fa190a7e10c07eb5a75371619.js"
+  "./precache-manifest.d2bfb8575fdfbf0f316357a1f0509f65.js"
 );
 
 self.addEventListener('message', (event) => {
