2560c611a0d2dfdaff31979e93a3d8b780e26780
Ingo Fischer <github@fischer-ka.de>
2020-11-06 22:49:40 +0100

    chore: release v2.3.1
    Prevent crash case (Sentry IOBROKER-SCENES-M)

---
+++ admin/precache-manifest.095ea239327b686ae4b29f4319cf7a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee1e9e283e20260210e4a9c5a4de68e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f79c1b964821698d7505",
+    "url": "./static/css/2.9ba4c952.chunk.css"
+  },
+  {
+    "revision": "2333d6173b1248ce6a6a",
+    "url": "./static/css/main.d4bed9c6.chunk.css"
+  },
+  {
+    "revision": "f79c1b964821698d7505",
+    "url": "./static/js/2.54b1c366.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "./static/js/2.54b1c366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2333d6173b1248ce6a6a",
+    "url": "./static/js/main.04654416.chunk.js"
+  },
+  {
+    "revision": "9aca8368f49fd341b64f",
+    "url": "./static/js/runtime-main.53966339.js"
+  }
+]);
--- admin/service-worker.js
+++ admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b116a35a4cf50785265e862885f25d01.js"
+  "./precache-manifest.095ea239327b686ae4b29f4319cf7a78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
