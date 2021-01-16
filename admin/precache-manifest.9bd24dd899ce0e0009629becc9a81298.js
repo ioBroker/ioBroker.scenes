689e3d03746a650ef5a9a0b4747d055fbde20750
Ingo Fischer <github@fischer-ka.de>
2021-01-16 17:13:24 +0100

    chore: release v2.3.4
    Prevent crash case (Sentry IOBROKER-SCENES-X, IOBROKER-SCENES-V)

---
+++ admin/precache-manifest.9bd24dd899ce0e0009629becc9a81298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de71f18804d8185a9d2c33beb3b2565e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d6befa6938b7ce7bcf0",
+    "url": "./static/css/2.7b005c66.chunk.css"
+  },
+  {
+    "revision": "f608c23e7c1ad8c2a0d2",
+    "url": "./static/css/main.d4bed9c6.chunk.css"
+  },
+  {
+    "revision": "8d6befa6938b7ce7bcf0",
+    "url": "./static/js/2.83c16503.chunk.js"
+  },
+  {
+    "revision": "5cf0b55f711b4765b7e42faa5df6636d",
+    "url": "./static/js/2.83c16503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f608c23e7c1ad8c2a0d2",
+    "url": "./static/js/main.0b158f37.chunk.js"
+  },
+  {
+    "revision": "9aca8368f49fd341b64f",
+    "url": "./static/js/runtime-main.53966339.js"
+  }
+]);
--- admin/service-worker.js
+++ admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.046fc793aac1d7c759e478278e6bf4c2.js"
+  "./precache-manifest.9bd24dd899ce0e0009629becc9a81298.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
