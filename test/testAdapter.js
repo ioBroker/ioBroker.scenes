a090db8ab9be617dc0a4b405a282c4789117e5d2
GermanBluefox <dogafox@gmail.com>
2025-01-23 14:29:04 +0000

    Fixed tests

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -1,15 +1,12 @@
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
-var expect = require('chai').expect;
-var setup  = require(__dirname + '/lib/setup');
+const expect = require('chai').expect;
+const setup = require('@iobroker/legacy-testing');
 
-var objects = null;
-var states  = null;
-var onStateChanged = null;
-var onObjectChanged = null;
-var sendToID = 1;
+let objects = null;
+let states = null;
+let onStateChanged = null;
+let sendToID = 1;
 
-var adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.')+1);
+const adapterShortName = setup.adapterName.substring(setup.adapterName.indexOf('.') + 1);
 
 function checkConnectionOfAdapter(cb, counter) {
     counter = counter || 0;
@@ -19,14 +16,16 @@ function checkConnectionOfAdapter(cb, counter) {
         return;
     }
 
-    states.getState('system.adapter.' + adapterShortName + '.0.alive', function (err, state) {
-        if (err) console.error(err);
-        if (state && state.val) {
-            if (cb) cb();
+    states.getState(`system.adapter.${adapterShortName}.0.alive`, (err, state) => {
+        if (err) {
+            console.error(err);
+        }
+        if (state?.val) {
+            if (cb) {
+                cb();
+            }
         } else {
-            setTimeout(function () {
-                checkConnectionOfAdapter(cb, counter + 1);
-            }, 1000);
+            setTimeout(() => checkConnectionOfAdapter(cb, counter + 1), 1000);
         }
     });
 }
@@ -34,93 +33,105 @@ function checkConnectionOfAdapter(cb, counter) {
 function checkValueOfState(id, value, cb, counter) {
     counter = counter || 0;
     if (counter > 20) {
-        if (cb) cb('Cannot check value Of State ' + id);
+        if (cb) cb(`Cannot check value Of State ${id}`);
         return;
     }
 
-    states.getState(id, function (err, state) {
-        if (err) console.error(err);
+    states.getState(id, (err, state) => {
+        if (err) {
+            console.error(err);
+        }
         if (value === null && !state) {
-            if (cb) cb();
-        } else
-        if (state && (value === undefined || state.val === value)) {
-            if (cb) cb();
+            if (cb) {
+                cb();
+            }
+        } else if (state && (value === undefined || state.val === value)) {
+            if (cb) {
+                cb();
+            }
         } else {
-            setTimeout(function () {
-                checkValueOfState(id, value, cb, counter + 1);
-            }, 500);
+            setTimeout(() => checkValueOfState(id, value, cb, counter + 1), 500);
         }
     });
 }
 
 function sendTo(target, command, message, callback) {
-    onStateChanged = function (id, state) {
+    onStateChanged = (id, state) => {
         if (id === 'messagebox.system.adapter.test.0') {
             callback(state.message);
         }
     };
 
     states.pushMessage('system.adapter.' + target, {
-        command:    command,
-        message:    message,
-        from:       'system.adapter.test.0',
+        command: command,
+        message: message,
+        from: 'system.adapter.test.0',
         callback: {
             message: message,
-            id:      sendToID++,
-            ack:     false,
-            time:    (new Date()).getTime()
-        }
+            id: sendToID++,
+            ack: false,
+            time: new Date().getTime(),
+        },
     });
 }
 
-describe('Test ' + adapterShortName + ' adapter', function() {
+describe('Test ' + adapterShortName + ' adapter', function () {
     before('Test ' + adapterShortName + ' adapter: Start js-controller', function (_done) {
-        this.timeout(600000); // because of first install from npm
+        this.timeout(600000); // because of first installation from npm
 
-        setup.setupController(async function () {
-            var config = await setup.getAdapterConfig();
+        setup.setupController(async () => {
+            const config = await setup.getAdapterConfig();
             // enable adapter
-            config.common.enabled  = true;
+            config.common.enabled = true;
             config.common.loglevel = 'debug';
 
             //config.native.dbtype   = 'sqlite';
 
             await setup.setAdapterConfig(config.common, config.native);
 
-            setup.startController(true, function(id, obj) {}, function (id, state) {
-                    if (onStateChanged) onStateChanged(id, state);
+            setup.startController(
+                true,
+                (id, obj) => {},
+                (id, state) => {
+                    if (onStateChanged) {
+                        onStateChanged(id, state);
+                    }
                 },
-                function (_objects, _states) {
+                (_objects, _states) => {
                     objects = _objects;
-                    states  = _states;
+                    states = _states;
                     _done();
-                });
+                },
+            );
         });
     });
 
-/*
+    /*
     ENABLE THIS WHEN ADAPTER RUNS IN DEAMON MODE TO CHECK THAT IT HAS STARTED SUCCESSFULLY
 */
-    it('Test ' + adapterShortName + ' adapter: Check if adapter started', function (done) {
+    it(`Test ${adapterShortName} adapter: Check if adapter started`, function (done) {
         this.timeout(60000);
-        checkConnectionOfAdapter(function (res) {
-            if (res) console.log(res);
+        checkConnectionOfAdapter(res => {
+            if (res) {
+                console.log(res);
+            }
             expect(res).not.to.be.equal('Cannot check connection');
-            objects.setObject('system.adapter.test.0', {
-                    common: {
-
-                    },
-                    type: 'instance'
+            objects.setObject(
+                'system.adapter.test.0',
+                {
+                    common: {},
+                    type: 'instance',
                 },
-                function () {
+                () => {
                     states.subscribeMessage('system.adapter.test.0');
                     done();
-                });
+                },
+            );
         });
     });
-/**/
+    /**/
 
-/*
+    /*
     PUT YOUR OWN TESTS HERE USING
     it('Testname', function ( done) {
         ...
@@ -129,11 +140,11 @@ describe('Test ' + adapterShortName + ' adapter', function() {
     You can also use "sendTo" method to send messages to the started adapter
 */
 
-    after('Test ' + adapterShortName + ' adapter: Stop js-controller', function (done) {
+    after(`Test ${adapterShortName} adapter: Stop js-controller`, function (done) {
         this.timeout(10000);
 
-        setup.stopController(function (normalTerminated) {
-            console.log('Adapter normal terminated: ' + normalTerminated);
+        setup.stopController(normalTerminated => {
+            console.log(`Adapter normal terminated: ${normalTerminated}`);
             done();
         });
     });
